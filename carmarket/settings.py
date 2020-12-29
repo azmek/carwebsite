17b8fbe5971d3aa058af684f82109a1c504f11a7
Aziz Meknassi <azizmek@gmail.com>
2020-12-29 10:33:05 +0000

    completed teams module

--- carmarket/settings.py
+++ carmarket/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'carmarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'carmarket_db',
+        'USER':'postgres',
+        'PASSWORD':'LefKada??01',
+        'Host': '127.0.0.1',
+        'PORT': '5434',
     }
 }
 
@@ -124,3 +128,6 @@ STATICFILES_DIRS = [
     "carmarket/static",
 
 ]
+#media settings
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
